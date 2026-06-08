3140dcbdcfb407f5709502abacdeaedf170a4aab
ashwinvisalpk <ashwinvisalpk@gmail.com>
2026-06-08 11:25:24 +0530

    Fix import statement for option_menu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_option_new import option_menu
+from streamlit_option_menu import option_menu
 st.title("my live application")
 with st.sidebar:
   data = option_menu(
