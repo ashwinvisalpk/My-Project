2a8be189f1f64a5d83607cb27c100ea0b1688625
ashwinvisalpk <ashwinvisalpk@gmail.com>
2026-06-08 11:21:31 +0530

    Fix typo in sidebar menu title

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from streamlit_option_new import option menu
+from streamlit_option_new import option_menu
 st.title("my live application")
 with st.sidebar:
   data = option_menu(
-  menu_title = "My Appa",
+  menu_title = "My Apps",
   options=["Home", "About","Services"],
   )
